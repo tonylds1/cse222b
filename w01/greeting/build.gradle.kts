1a3511f00aadad82247ffc50147ee340012f9896
Tony Moraes <idd_tmoraes@uolinc.com>
2024-09-15 17:47:45 -0300

    week01 - first commit

---
+++ w01/greeting/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    kotlin("jvm") version "2.0.10"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "org.example.MainKt"
+    }
+    configurations["compileClasspath"].forEach { file: File ->
+        from(zipTree(file.absoluteFile))
+    }
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+}
